c2b93c1f013ed356c5e6fb21de58f7a72b30211d
viju shankar <vijushankar@gmail.com>
2023-06-29 21:25:08 +0530

    exp04 commit changed to while loop

--- exp04.py
+++ exp04.py
@@ -14,7 +14,7 @@ Problem : To find sum of elements in a list using while loop ( C style loop)
 EXP 04
 GPC VENNIKULAM
 """
-
+"""
 def findSum(mylist):
   s = 0;
   for no in mylist:
@@ -22,3 +22,21 @@ def findSum(mylist):
   return s
   
 print("The sum is",findSum([1, 2, 4]))
+"""
+# Initialize a list of numbers
+numbers = [1, 2, 3, 4, 5]
+
+# Initialize variables for sum and index
+total_sum = 0
+index = 0
+
+# Calculate the sum using a while loop
+while index < len(numbers):
+    total_sum += numbers[index]
+    index += 1
+
+# Print the sum
+print("The list is:", numbers)
+
+# Print the sum
+print("Sum of elements:", total_sum)
