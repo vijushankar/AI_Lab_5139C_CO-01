38e09b35c1cd5925415803908717558c4e2bb016
viju shankar <vijushankar@gmail.com>
2023-06-29 18:26:30 +0530

    exp03

---
+++ exp03.py
@@ -0,0 +1,18 @@
+"""
+Develop simple python programs using decision control structures
+
+Problem : To determine if the current minute is odd or even
+Ref : Head Firs Java, Paul Barry
+
+GPC VENNIKULAM
+EXP 03
+"""
+
+from datetime import datetime
+
+odds = list (range(1,60))
+right_this_minute = datetime.today().minute
+if right_this_minute in odds :
+  print("This minute seems a little odd.")
+else:
+  print("Not an odd minute.")
